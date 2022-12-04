e469478c3e6e94d257249b86a2ca7eb0686a2ebe
zaba505 <cakub6@gmx.com>
2022-12-04 18:15:08 -0500

    feat(service/showmgmt): roughed out base http service

---
+++ services/showmgmt/cmd/serve_http.go
@@ -0,0 +1,72 @@
+// Copyright 2022 Z5Labs and Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"net"
+
+	"github.com/z5labs/griot/services/showmgmt/http"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
+
+func withServeHttpCmd() func(*viper.Viper) *cobra.Command {
+	return func(v *viper.Viper) *cobra.Command {
+		cmd := &cobra.Command{
+			Use:               "http",
+			PersistentPreRunE: withPersistentPreRun()(v),
+			RunE: func(cmd *cobra.Command, args []string) error {
+				addr := v.GetString("addr")
+				ls, err := net.Listen("tcp", addr)
+				if err != nil {
+					zap.L().Error("failed to initialize network listener", zap.String("addr", addr), zap.Error(err))
+					return Error{
+						Cmd:   cmd,
+						Cause: err,
+					}
+				}
+
+				s, err := http.NewShowMgmtService(http.ServiceConfig{
+					Logger: zap.L(),
+				})
+				if err != nil {
+					zap.L().Error("failed to initialize show mgmt service", zap.String("addr", addr), zap.Error(err))
+					return Error{
+						Cmd:   cmd,
+						Cause: err,
+					}
+				}
+
+				err = s.Serve(cmd.Context(), ls)
+				if err == nil {
+					return nil
+				}
+				zap.L().Error("unexpected error while serving requests", zap.String("addr", addr), zap.Error(err))
+				return Error{
+					Cmd:   cmd,
+					Cause: err,
+				}
+			},
+		}
+
+		cmd.Flags().String("addr", ":0", "")
+
+		v.BindPFlag("addr", cmd.Flags().Lookup("addr"))
+
+		return cmd
+	}
+}
