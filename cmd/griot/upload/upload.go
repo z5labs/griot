3e32f886e326f3fdf7a577147f359fbc2a40125a
Richard Carson Derr <cakub6@gmx.com>
2024-10-16 00:14:11 -0400

    feat(issue-33): fully implement upload command

--- cmd/griot/upload/upload.go
+++ cmd/griot/upload/upload.go
@@ -16,12 +16,22 @@ package upload
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"hash"
 	"io"
 	"log/slog"
+	"os"
+	"strings"
+
+	"github.com/z5labs/griot/cmd/internal/command"
+	"github.com/z5labs/griot/services/content"
+	"github.com/z5labs/griot/services/content/contentpb"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
-	"github.com/z5labs/griot/cmd/internal/command"
+	"go.opentelemetry.io/otel"
 )
 
 func New() *cobra.Command {
@@ -32,6 +42,7 @@ func New() *cobra.Command {
 			fs.String("name", "", "Provide an optional name to help identify this content later.")
 			fs.String("media-type", "", "Specify the content Media Type.")
 			fs.String("source-file", "", "Specify the content source file.")
+			fs.String("hash-func", "sha256", "Specify hash function used for calculating content checksum.")
 		}),
 		command.Handle(initUploadHandler),
 	)
@@ -43,6 +54,11 @@ type config struct {
 	Name       string `flag:"name"`
 	MediaType  string `flag:"media-type"`
 	SourceFile string `flag:"source-file"`
+	HashFunc   string `flag:"hash-func"`
+}
+
+type uploadClient interface {
+	UploadContent(context.Context, *content.UploadContentRequest) (*content.UploadContentResponse, error)
 }
 
 type handler struct {
@@ -50,18 +66,79 @@ type handler struct {
 
 	contentName string
 	mediaType   string
-	filename    string
+	hasher      hash.Hash
+	src         io.ReadSeeker
 	out         io.Writer
+
+	content uploadClient
 }
 
 func initUploadHandler(ctx context.Context, cfg config) (command.Handler, error) {
+	var hasher hash.Hash
+	switch cfg.HashFunc {
+	case "sha256":
+		hasher = sha256.New()
+	default:
+		return nil, fmt.Errorf("unsupported hash function: %s", cfg.HashFunc)
+	}
+
+	src, err := openSourceFile(cfg.SourceFile)
+	if err != nil {
+		return nil, err
+	}
+
 	h := &handler{
-		log: command.Logger(cfg.LoggingConfig),
+		log:         command.Logger(cfg.LoggingConfig),
+		contentName: cfg.Name,
+		mediaType:   cfg.MediaType,
+		hasher:      hasher,
+		src:         src,
+		out:         os.Stdout,
+		content:     content.NewClient(),
 	}
 	return h, nil
 }
 
 func (h *handler) Handle(ctx context.Context) error {
-	h.log.InfoContext(ctx, "hello")
-	return nil
+	spanCtx, span := otel.Tracer("upload").Start(ctx, "handler.Handle")
+	defer span.End()
+
+	_, err := io.Copy(h.hasher, h.src)
+	if err != nil {
+		h.log.ErrorContext(spanCtx, "failed to compute hash", slog.String("error", err.Error()))
+		return err
+	}
+
+	_, err = h.src.Seek(0, 0)
+	if err != nil {
+		h.log.ErrorContext(spanCtx, "failed to seek to start of content source", slog.String("error", err.Error()))
+		return err
+	}
+
+	req := &content.UploadContentRequest{
+		Metadata: &contentpb.Metadata{
+			Name: &h.contentName,
+			Checksum: &contentpb.Checksum{
+				HashFunc: contentpb.HashFunc_SHA256.Enum(),
+				Hash:     h.hasher.Sum(nil),
+			},
+		},
+		Body: h.src,
+	}
+	resp, err := h.content.UploadContent(spanCtx, req)
+	if err != nil {
+		h.log.ErrorContext(spanCtx, "failed to upload content", slog.String("error", err.Error()))
+		return err
+	}
+
+	enc := json.NewEncoder(h.out)
+	return enc.Encode(resp)
+}
+
+func openSourceFile(filename string) (*os.File, error) {
+	filename = strings.TrimSpace(filename)
+	if len(filename) == 0 {
+		return os.Stdin, nil
+	}
+	return os.Open(filename)
 }
