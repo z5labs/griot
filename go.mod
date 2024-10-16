21691cd7b2f9c8f5340aad5c5a4412830d1ab173
Richard Carson Derr <cakub6@gmx.com>
2024-10-16 00:13:28 -0400

    feat(issue-33): rough out content client

--- go.mod
+++ go.mod
@@ -6,7 +6,14 @@ require (
 	github.com/go-viper/mapstructure/v2 v2.2.1
 	github.com/spf13/cobra v1.8.1
 	github.com/spf13/pflag v1.0.5
+	go.opentelemetry.io/otel v1.31.0
 	google.golang.org/protobuf v1.35.1
 )
 
-require github.com/inconshreveable/mousetrap v1.1.0 // indirect
+require (
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	go.opentelemetry.io/otel/metric v1.31.0 // indirect
+	go.opentelemetry.io/otel/trace v1.31.0 // indirect
+)
---
+++ services/content/content.go
@@ -0,0 +1,47 @@
+// Copyright 2024 Z5Labs and Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package content
+package content
+
+import (
+	"context"
+	"io"
+
+	"github.com/z5labs/griot/services/content/contentpb"
+
+	"go.opentelemetry.io/otel"
+)
+
+type Client struct{}
+
+func NewClient() *Client {
+	c := &Client{}
+	return c
+}
+
+type UploadContentRequest struct {
+	Metadata *contentpb.Metadata
+	Body     io.Reader
+}
+
+type UploadContentResponse struct {
+	Id string `json:"id"`
+}
+
+func (c *Client) UploadContent(ctx context.Context, req *UploadContentRequest) (*UploadContentResponse, error) {
+	_, span := otel.Tracer("content").Start(ctx, "Client.UploadContent")
+	defer span.End()
+	return nil, nil
+}
